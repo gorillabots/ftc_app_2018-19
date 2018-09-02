325300af5ccf3d219989a293f0bb07c4866a99d3
Mikko Neukirch <neukirchm@stny.rr.com>
2018-09-02 14:28:25 -0400

    Created test OpMode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOpMode.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(group="Tests", name="TestOpMode")
+public class TestOpMode extends LinearOpMode
+{
+    public void runOpMode()
+    {
+        telemetry.addData("Status", "Initialized");
+        waitForStart();
+        telemetry.addData("Status", "Started");
+    }
+}
