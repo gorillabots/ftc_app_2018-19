9ee376475a96d38266af76caa83f62d39a30e20e
Mikko Neukirch <neukirchm@protonmail.ch>
2018-09-14 18:54:24 -0400

    Added telemetry update that I forgot earlier

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOpMode.java
@@ -9,9 +9,12 @@ public class TestOpMode extends LinearOpMode
     public void runOpMode()
     {
         telemetry.addData("Status", "Initialized");
+        telemetry.update();
         waitForStart();
         telemetry.addData("Status", "Started");
+        telemetry.update();
         sleep(1000);
         telemetry.addData("Status", "Done");
+        telemetry.update();
     }
 }
