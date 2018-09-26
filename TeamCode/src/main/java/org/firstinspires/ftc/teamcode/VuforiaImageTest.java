bfec3e0bcffc7aa2e96e32c6a3044c15618c82c1
Mikko Neukirch <neukirchm@protonmail.ch>
2018-09-25 20:20:52 -0400

    Added vuforia image test (doesn't work)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VuforiaImageTest.java
@@ -0,0 +1,88 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.content.res.Resources;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.vuforia.Frame;
+import com.vuforia.Image;
+import com.vuforia.PIXEL_FORMAT;
+import com.vuforia.Vuforia;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.concurrent.BlockingQueue;
+
+@Autonomous(group="Test", name="Vuforia Image Test")
+public class VuforiaImageTest extends LinearOpMode
+{
+    VuforiaLocalizer vuforia;
+
+    public void runOpMode()
+    {
+        Vuforia.setFrameFormat(PIXEL_FORMAT.RGB565, true);
+
+        Context ctx = hardwareMap.appContext;
+        Resources resources = ctx.getResources();
+        int camera = resources.getIdentifier("cameraMonitorViewId", "id", ctx.getPackageName());
+
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(camera);
+        parameters.vuforiaLicenseKey = getVuforiaKey();
+        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+
+        vuforia = ClassFactory.createVuforiaLocalizer(parameters);
+
+        telemetry.addData("Status", "Initialized");
+        tel
+
+        waitForStart();
+
+        BlockingQueue<VuforiaLocalizer.CloseableFrame> frameQueue = vuforia.getFrameQueue();
+        Frame frame;
+        Image img;
+
+        while(opModeIsActive())
+        {
+            try
+            {
+                frame = frameQueue.take();
+            }
+            catch(InterruptedException e)
+            {
+                e.printStackTrace();
+                break;
+            }
+
+            img = frame.getImage(1);
+            byte[] pxls = img.getPixels().array();
+
+            telemetry.addData("Color", pxls[0] + "," + pxls[1] + "," + pxls[2]);
+            telemetry.update();
+        }
+    }
+
+    String getVuforiaKey()
+    {
+        try
+        {
+            AssetManager am = hardwareMap.appContext.getAssets();
+            InputStream is = am.open("vuforiakey.txt");
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String key = br.readLine();
+            return key;
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
