6dc5e83d124b52d4b63804cf1cf65059eba957cd
Andy He <xia.yifu82@gmail.com>
2018-10-07 12:55:42 -0400

    worked on color sensor stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousOpMode.java
@@ -5,12 +5,30 @@ import com.qualcomm.robotcore.hardware.ColorSensor;
 import org.firstinspires.ftc.teamcode.old.OldAutonomousOpMode;
 
 public abstract class AutonomousOpMode extends OldAutonomousOpMode {
+    protected ColorSensor color;
 
-    public void initializeAutonomous(){
-
+    public void initializeAutonomous() {
+        color = hardwareMap.get(ColorSensor.class, "color");
     }
-    public void unhang(){
 
+    public void unhang() {
+        //THIRTY POINTS!!!!!!!!!!!!!
+    }
+
+    public boolean isRedGround() {
+        return color.red() >= 1000;
+    }
+
+    public boolean isBlueGround() {
+        return color.blue() >= 1000;
+    }
+
+    public void MoveToUntil(boolean isTrue, double direction, double power) {
+        while (!isTrue && opModeIsActive()) {
+            motors.MoveTo(direction, power);
+            
+        }
+        motors.stopMotors();
     }
 
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/SensorTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/SensorTesting.java
@@ -1,34 +1,34 @@
 package org.firstinspires.ftc.teamcode.tests;
 
-import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.ColorSensor;
-import com.qualcomm.robotcore.hardware.DistanceSensor;
 
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.AutonomousOpMode;
 
 
 @Autonomous(group="test", name="SensorTesting")
-public class SensorTesting extends LinearOpMode
+public class SensorTesting extends AutonomousOpMode
 {
     //private DistanceSensor distance;
-    private ColorSensor color;
+
     @Override
     public void runOpMode()
     {
-      //  distance = hardwareMap.get(DistanceSensor.class, "distance");
-        color = hardwareMap.get(ColorSensor.class,"color");
+        initializeAutonomous();
+
         waitForStart();
+
         while(opModeIsActive())
         {
-       //     telemetry.addData("D", distance.getDistance(DistanceUnit.INCH));
+         // telemetry.addData("D", distance.getDistance(DistanceUnit.INCH));
             telemetry.addData("B", color.blue());
             telemetry.addData("R", color.red());
             telemetry.addData("G", color.green());
             telemetry.addData("alpha", color.alpha());
             telemetry.addData("ARGB", color.argb());
+            telemetry.addData("isRedGround" , isRedGround());
+            telemetry.addData("isBlueGround", isBlueGround());
             telemetry.update();
+
         }
     }
 }
